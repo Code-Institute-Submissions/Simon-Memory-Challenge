3fc83aaf80ed08a62156a5cfa92e9b5ef54c3af2
Fatima <fatimasmails@gmail.com>
2020-12-18 22:54:59 +0000

    Add function to start the game in game.js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,30 @@
+
+//---------------------------------------------------------Game button variables
+
 var buttonColours = ["red", "blue", "green", "yellow"];
+
+var gamePattern = [];
+var userClickedPattern = [];
+
+var started = false;
+var level = 0;
+
+//----------------------------------------------------------------Game Functions
+
+$("#btnStart").click(function () {
+    //    console.log('test -  ', level, started);
+    if (!started) {
+        $("#level-title").text("Level " + level);
+        $(this).addClass("started");
+        $("#btnTextPlay").text("TRY AGAIN");
+        nextSequence();
+        started = true;
+    } else {
+        level = 0;
+        gamePattern = [];
+        started = false;
+        $(this).removeClass("started");
+        $("#level-title").text("Press start to play game");
+        $("#btnTextPlay").text("START");
+    }
+});
