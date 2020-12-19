b7a27a085c7edb2e8e328d83bf715331a395ecee
Fatima <fatimasmails@gmail.com>
2020-12-19 00:29:17 +0000

    Add functions for game sequence in 'game.js' file

--- assets/js/game.js
+++ assets/js/game.js
@@ -62,3 +62,38 @@ function checkAnswer(currentLevel) {
         startOver();
     }
 }
+
+//Function for game sequence
+
+function nextSequence() {
+    userClickedPattern = [];
+    level++;
+    $("#level-title").text("Level " + level);
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColour);
+}
+
+function animatePress(currentColor) {
+    $("#" + currentColor).addClass("pressed");
+    setTimeout(function () {
+        $("#" + currentColor).removeClass("pressed");
+    }, 100);
+}
+
+function playSound(name) {
+    // Play if sound is not mute
+    if (!$('.btn-sound').hasClass('sound-mute')) {
+        var audio = new Audio("sounds/" + name + ".mp3");
+        audio.play();
+    }
+}
+
+function startOver() {
+    level = 0;
+    gamePattern = [];
+    started = false;
+}
